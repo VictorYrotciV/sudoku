fe80744790e538c651e6da6dd5a183983c31ee02
Sinsoledad1227 <nkupsl@163.com>
2023-06-22 15:34:52 +0800

    psl

--- lib/save_game.h
+++ lib/save_game.h
@@ -9,33 +9,38 @@ void saveSudokuGamesToFile(const std::vector<std::vector<std::vector<int>>>& gam
 void generateAndSaveSudokuGames(int n, int r, const std::string& file_path);
 
 
-void saveSudokuGamesToFile(const std::vector<std::vector<std::vector<int>>>& games, const std::string& file_path) {
+void saveSudokuGamesToFile(const std::vector<std::vector<std::vector<int>>>& games, const std::string& file_path)
+{
     std::ofstream file(file_path);
 
-    if (!file) {
+    if (!file)
+    {
         std::cerr << "file error"<< file_path << std::endl;
         return;
     }
 
-    for (const auto& game : games) {
-        for (int row = 0; row < SIZE; row++) {
-            for (int col = 0; col < SIZE; col++) {
+    for (const auto& game : games)
+    {
+        for (int row = 0; row < SIZE; row++)
+        {
+            for (int col = 0; col < SIZE; col++)
+            {
                 file << game[row][col] << " ";
             }
             file << std::endl;
         }
         file << std::endl;
     }
-
     file.close();
 
     std::cout << "save path:"<< file_path << std::endl;
 }
 
-void generateAndSaveSudokuGames(int n, int r, const std::string& file_path) {
+void generateAndSaveSudokuGames(int n, int r, const std::string& file_path)
+{
     std::vector<std::vector<std::vector<int>>> sudoku_games;
-
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++)
+    {
         std::vector<std::vector<int>> sudoku = generateSudokuGameBoard(r);
         sudoku_games.push_back(sudoku);
     }
