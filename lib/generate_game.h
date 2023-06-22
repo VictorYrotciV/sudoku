bddea3493dcf3921688750079b7978474f2021e3
Sinsoledad1227 <nkupsl@163.com>
2023-06-22 15:37:34 +0800

    psl

--- lib/generate_game.h
+++ lib/generate_game.h
@@ -1,6 +1,3 @@
-#ifndef GENERATE_GAME_H
-#define GENERATE_GAME_H
-
 #include <iostream>
 #include <vector>
 #include <random>
@@ -18,9 +15,12 @@ std::vector<std::vector<int>> generateSudokuGameBoard(int r);
 std::vector<std::vector<int>> generateSudokuSolutionOnlyGameBoard(int r);
 
 // 检查数字在指定行是否重复
-bool isRowValid(const std::vector<std::vector<int>>& grid, int row, int num) {
-    for (int col = 0; col < SIZE; col++) {
-        if (grid[row][col] == num) {
+bool isRowValid(const std::vector<std::vector<int>>& grid, int row, int num)
+{
+    for (int col = 0; col < SIZE; col++)
+    {
+        if (grid[row][col] == num)
+        {
             return false;
         }
     }
@@ -28,9 +28,12 @@ bool isRowValid(const std::vector<std::vector<int>>& grid, int row, int num) {
 }
 
 // 检查数字在指定列是否重复
-bool isColValid(const std::vector<std::vector<int>>& grid, int col, int num) {
-    for (int row = 0; row < SIZE; row++) {
-        if (grid[row][col] == num) {
+bool isColValid(const std::vector<std::vector<int>>& grid, int col, int num)
+{
+    for (int row = 0; row < SIZE; row++)
+    {
+        if (grid[row][col] == num)
+        {
             return false;
         }
     }
@@ -38,10 +41,14 @@ bool isColValid(const std::vector<std::vector<int>>& grid, int col, int num) {
 }
 
 // 检查数字在指定的小方块是否重复
-bool isBoxValid(const std::vector<std::vector<int>>& grid, int startRow, int startCol, int num) {
-    for (int row = 0; row < 3; row++) {
-        for (int col = 0; col < 3; col++) {
-            if (grid[row + startRow][col + startCol] == num) {
+bool isBoxValid(const std::vector<std::vector<int>>& grid, int startRow, int startCol, int num)
+{
+    for (int row = 0; row < 3; row++)
+    {
+        for (int col = 0; col < 3; col++)
+        {
+            if (grid[row + startRow][col + startCol] == num)
+            {
                 return false;
             }
         }
@@ -50,24 +57,29 @@ bool isBoxValid(const std::vector<std::vector<int>>& grid, int startRow, int sta
 }
 
 // 检查数字在指定位置是否满足数独规则
-bool isValid(const std::vector<std::vector<int>>& grid, int row, int col, int num) {
+bool isValid(const std::vector<std::vector<int>>& grid, int row, int col, int num)
+{
     return isRowValid(grid, row, num) && isColValid(grid, col, num) && isBoxValid(grid, row - row % 3, col - col % 3, num);
 }
 
 // 在指定位置填充数字
-bool fillNumber(std::vector<std::vector<int>>& grid, int row, int col) {
+bool fillNumber(std::vector<std::vector<int>>& grid, int row, int col)
+{
     // 数独已填满，返回 true 表示成功生成终盘
-    if (row == SIZE) {
+    if (row == SIZE)
+    {
         return true;
     }
 
     // 列遍历完成，转到下一行
-    if (col == SIZE) {
+    if (col == SIZE)
+    {
         return fillNumber(grid, row + 1, 0);
     }
 
     // 如果当前位置已经有数字，跳到下一列
-    if (grid[row][col] != 0) {
+    if (grid[row][col] != 0)
+    {
         return fillNumber(grid, row, col + 1);
     }
 
@@ -78,10 +90,13 @@ bool fillNumber(std::vector<std::vector<int>>& grid, int row, int col) {
     std::shuffle(nums.begin(), nums.end(), rng);
 
     // 尝试在当前位置填入符合规则的数字
-    for (int num : nums) {
-        if (isValid(grid, row, col, num)) {
+    for (int num : nums)
+    {
+        if (isValid(grid, row, col, num))
+        {
             grid[row][col] = num;
-            if (fillNumber(grid, row, col + 1)) {
+            if (fillNumber(grid, row, col + 1))
+            {
                 return true;
             }
             grid[row][col] = 0;
@@ -92,7 +107,8 @@ bool fillNumber(std::vector<std::vector<int>>& grid, int row, int col) {
 }
 
 // 生成数独终盘
-std::vector<std::vector<int>> generateSudokuFinalBoard() {
+std::vector<std::vector<int>> generateSudokuFinalBoard()
+{
     std::vector<std::vector<int>> grid(SIZE, std::vector<int>(SIZE, 0));
 
     // 填充数独
@@ -102,7 +118,8 @@ std::vector<std::vector<int>> generateSudokuFinalBoard() {
 }
 
 //挖空法生成数独游戏
-std::vector<std::vector<int>> generateSudokuGameBoard(int r) {
+std::vector<std::vector<int>> generateSudokuGameBoard(int r)
+{
     std::vector<std::vector<int>> grid(SIZE, std::vector<int>(SIZE, 0));
 
     // 填充数独
@@ -114,11 +131,13 @@ std::vector<std::vector<int>> generateSudokuGameBoard(int r) {
     std::uniform_int_distribution<int> distribution(0, SIZE - 1);
 
     int count = 0;
-    while (count < r) {
+    while (count < r)
+    {
         int row = distribution(rng);
         int col = distribution(rng);
 
-        if (grid[row][col] != 0) {
+        if (grid[row][col] != 0)
+        {
             grid[row][col] = 0;
             count++;
         }
@@ -127,7 +146,8 @@ std::vector<std::vector<int>> generateSudokuGameBoard(int r) {
     return grid;
 }
 
-std::vector<std::vector<int>> generateSudokuSolutionOnlyGameBoard(int r) {
+std::vector<std::vector<int>> generateSudokuSolutionOnlyGameBoard(int r)
+{
     std::vector<std::vector<int>> grid(SIZE, std::vector<int>(SIZE, 0));
 
     // 填充数独
@@ -142,11 +162,13 @@ std::vector<std::vector<int>> generateSudokuSolutionOnlyGameBoard(int r) {
     int maxAttempts = r * 10; // 设置最大尝试次数
     int attempts = 0;
 
-    while (count < r) {
+    while (count < r)
+    {
         int row = distribution(rng);
         int col = distribution(rng);
 
-        if (grid[row][col] != 0) {
+        if (grid[row][col] != 0)
+        {
             int temp = grid[row][col];
             grid[row][col] = 0;
             count++;
@@ -155,11 +177,13 @@ std::vector<std::vector<int>> generateSudokuSolutionOnlyGameBoard(int r) {
             std::vector<std::vector<int>> copyGrid = grid;
             int solutionCount = 0;
 
-            solveSudoku(copyGrid, [&]() {
+            solveSudoku(copyGrid, [&]()
+            {
                 solutionCount++;
             });
 
-            if (solutionCount != 1) {
+            if (solutionCount != 1)
+            {
                 // 如果有多于一个解，则恢复原来的数字
                 grid[row][col] = temp;
                 count--;
@@ -167,7 +191,8 @@ std::vector<std::vector<int>> generateSudokuSolutionOnlyGameBoard(int r) {
 
             attempts++;
 
-            if (attempts >= maxAttempts) {
+            if (attempts >= maxAttempts)
+            {
                 // 尝试次数超过阈值，返回当前数独游戏并打印警告
                 std::cout << "未能生成挖空 " << r << " 个数的解唯一的数独游戏。当前挖空数量：" << count << std::endl;
                 return grid;
@@ -181,14 +206,14 @@ std::vector<std::vector<int>> generateSudokuSolutionOnlyGameBoard(int r) {
 
 
 // 打印数独
-void printSudokuBoard(const std::vector<std::vector<int>>& grid) {
-    for (int row = 0; row < SIZE; row++) {
-        for (int col = 0; col < SIZE; col++) {
+void printSudokuBoard(const std::vector<std::vector<int>>& grid)
+{
+    for (int row = 0; row < SIZE; row++)
+    {
+        for (int col = 0; col < SIZE; col++)
+        {
             std::cout << grid[row][col] << " ";
         }
         std::cout << std::endl;
     }
 }
-
-
-#endif
--- lib/solver.h
+++ lib/solver.h
@@ -1,6 +1,3 @@
-#ifndef SOLVER_H
-#define SOLVER_H
-
 #include <iostream>
 #include <fstream>
 #include <vector>
@@ -14,31 +11,38 @@ bool findEmptyLocation(const std::vector<std::vector<int>>& grid, int& row, int&
 bool isSafe(const std::vector<std::vector<int>>& grid, int row, int col, int num);
 int solveSudokuAndPrintAllSolutions(const std::vector<std::vector<int>>& grid, const std::string& file_path);
 
-bool solveSudoku(std::vector<std::vector<int>>& grid, std::function<void()> handleSolution) {
+bool solveSudoku(std::vector<std::vector<int>>& grid, std::function<void()> handleSolution)
+{
     int row, col;
-    if (!findEmptyLocation(grid, row, col)) {
-        // ÕÒµ½ÁËÒ»¸ö½â
+    if (!findEmptyLocation(grid, row, col))
+    {
+        // ï¿½Òµï¿½ï¿½ï¿½Ò»ï¿½ï¿½ï¿½ï¿½
         handleSolution();
         return true;
     }
 
-    for (int num = 1; num <= 9; num++) {
-        if (isSafe(grid, row, col, num)) {
+    for (int num = 1; num <= 9; num++)
+    {
+        if (isSafe(grid, row, col, num))
+        {
             grid[row][col] = num;
 
             if (solveSudoku(grid, handleSolution))
                 return true;
 
-            grid[row][col] = 0; // »ØËÝ£¬½«µ±Ç°Î»ÖÃÖØÖÃÎª0
+            grid[row][col] = 0; // ï¿½ï¿½ï¿½Ý£ï¿½ï¿½ï¿½ï¿½ï¿½Ç°Î»ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½Îª0
         }
     }
 
     return false;
 }
 
-bool findEmptyLocation(const std::vector<std::vector<int>>& grid, int& row, int& col) {
-    for (row = 0; row < SIZE; row++) {
-        for (col = 0; col < SIZE; col++) {
+bool findEmptyLocation(const std::vector<std::vector<int>>& grid, int& row, int& col)
+{
+    for (row = 0; row < SIZE; row++)
+    {
+        for (col = 0; col < SIZE; col++)
+        {
             if (grid[row][col] == 0)
                 return true;
         }
@@ -46,24 +50,29 @@ bool findEmptyLocation(const std::vector<std::vector<int>>& grid, int& row, int&
     return false;
 }
 
-bool isSafe(const std::vector<std::vector<int>>& grid, int row, int col, int num) {
-    // ¼ì²éÐÐÊÇ·ñ°²È«
-    for (int i = 0; i < SIZE; i++) {
+bool isSafe(const std::vector<std::vector<int>>& grid, int row, int col, int num)
+{
+    // ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½Ç·ï¿½È«
+    for (int i = 0; i < SIZE; i++)
+    {
         if (grid[row][i] == num)
             return false;
     }
 
-    // ¼ì²éÁÐÊÇ·ñ°²È«
-    for (int i = 0; i < SIZE; i++) {
+    // ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½Ç·ï¿½È«
+    for (int i = 0; i < SIZE; i++)
+    {
         if (grid[i][col] == num)
             return false;
     }
 
-    // ¼ì²é3x3·½¸ñÊÇ·ñ°²È«
+    // ï¿½ï¿½ï¿½3x3ï¿½ï¿½ï¿½ï¿½ï¿½Ç·ï¿½È«
     int startRow = row - row % 3;
     int startCol = col - col % 3;
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
             if (grid[i + startRow][j + startCol] == num)
                 return false;
         }
@@ -73,22 +82,26 @@ bool isSafe(const std::vector<std::vector<int>>& grid, int row, int col, int num
 }
 
 
-int solveSudokuAndPrintAllSolutions(const std::vector<std::vector<int>>& grid, const std::string& file_path) {
+int solveSudokuAndPrintAllSolutions(const std::vector<std::vector<int>>& grid, const std::string& file_path)
+{
     std::ofstream file(file_path);
-    if (!file.is_open()) {
+    if (!file.is_open())
+    {
         std::cout << "Failed to open the file: " << file_path << std::endl;
         return 0;
     }
 
     int solutionCount = 0;
-    std::vector<std::vector<int>> copyGrid = grid; // ¸´ÖÆÊý¶ÀÓÎÏ·
+    std::vector<std::vector<int>> copyGrid = grid; // ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½Ï·
 
-    // ¶¨ÒåÒ»¸ö»Øµ÷º¯ÊýÀ´´¦ÀíÃ¿¸ö½â
-    std::function<void()> handleSolution = [&]() {
+    // ï¿½ï¿½ï¿½ï¿½Ò»ï¿½ï¿½ï¿½Øµï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½Ã¿ï¿½ï¿½ï¿½ï¿½
+    std::function<void()> handleSolution = [&]()
+    {
         solutionCount++;
         file << "Solution " << solutionCount << ":\n";
         for (const auto& row : copyGrid) {
-            for (int num : row) {
+            for (int num : row)
+            {
                 file << num << " ";
             }
             file << "\n";
@@ -96,20 +109,10 @@ int solveSudokuAndPrintAllSolutions(const std::vector<std::vector<int>>& grid, c
         file << "\n";
     };
 
-    // µ÷ÓÃ solveSudoku º¯ÊýÀ´½â¾öÊý¶ÀÓÎÏ·£¬²¢ÔÚ»Øµ÷º¯ÊýÖÐ´¦ÀíÃ¿¸ö½â
+    // ï¿½ï¿½ï¿½ï¿½ solveSudoku ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½Ï·ï¿½ï¿½ï¿½ï¿½ï¿½Ú»Øµï¿½ï¿½ï¿½ï¿½ï¿½ï¿½Ð´ï¿½ï¿½ï¿½Ã¿ï¿½ï¿½ï¿½ï¿½
     solveSudoku(copyGrid, handleSolution);
 
     file.close();
 
     return solutionCount;
 }
-
-
-
-
-
-
-
-
-
-#endif
