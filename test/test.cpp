693fb7ea93f684b8254f3460617f3c071f036b62
Sinsoledad1227 <nkupsl@163.com>
2023-06-29 18:24:37 +0800

    googletest

--- test/test.cpp
+++ test/test.cpp
@@ -93,13 +93,25 @@ bool isRight(const std::vector<std::vector<int>> &grid,
     return true;
 }
 
+int getEmptyNum(const std::vector<std::vector<int>> &grid) {
+    int count = 0;
+    for (int row = 0; row < SIZE; row++) {
+        for (int col = 0; col < SIZE; col++) {
+            if (grid[row][col] == 0) {
+                count++;
+            }
+        }
+    }
+    return count;
+}
+
 TEST(TestCases, correctness_case_1) {
-     std::string games_file_path = DEFAULT_GAME_FILE_PATH;
-     std::string temp_file_path = DEFAULT_TEMP_FILE_PATH;
-     int game_num = 1,
-             game_level = DEFAULT_LEVEL,
-             r_lower_num = DEFAULT_R_LOWER,
-             r_upper_num = DEFAULT_R_UPPER;
+    std::string games_file_path = DEFAULT_GAME_FILE_PATH;
+    std::string temp_file_path = DEFAULT_TEMP_FILE_PATH;
+    int game_num = 1,
+            game_level = DEFAULT_LEVEL,
+            r_lower_num = DEFAULT_R_LOWER,
+            r_upper_num = DEFAULT_R_UPPER;
     genAndSaveGameBoards(game_num, r_lower_num, r_upper_num,
                          game_level, games_file_path);
     std::vector<std::vector<std::vector<int>>> boards = loadBoard(games_file_path);
@@ -120,15 +132,15 @@ TEST(TestCases, correctness_case_1) {
         }
     }
 
- }
+}
 
 TEST(TestCases, correctness_case_5) {
-     std::string games_file_path = DEFAULT_GAME_FILE_PATH;
-     std::string temp_file_path = DEFAULT_TEMP_FILE_PATH;
-     int game_num = 5,
-             game_level = DEFAULT_LEVEL,
-             r_lower_num = DEFAULT_R_LOWER,
-             r_upper_num = DEFAULT_R_UPPER;
+    std::string games_file_path = DEFAULT_GAME_FILE_PATH;
+    std::string temp_file_path = DEFAULT_TEMP_FILE_PATH;
+    int game_num = 5,
+            game_level = DEFAULT_LEVEL,
+            r_lower_num = DEFAULT_R_LOWER,
+            r_upper_num = DEFAULT_R_UPPER;
     genAndSaveGameBoards(game_num, r_lower_num, r_upper_num,
                          game_level, games_file_path);
     std::vector<std::vector<std::vector<int>>> boards = loadBoard(games_file_path);
@@ -149,15 +161,15 @@ TEST(TestCases, correctness_case_5) {
         }
     }
 
- }
+}
 
- TEST(TestCases, correctness_case_10) {
-     std::string games_file_path = DEFAULT_GAME_FILE_PATH;
-     std::string temp_file_path = DEFAULT_TEMP_FILE_PATH;
-     int game_num = 10,
-             game_level = DEFAULT_LEVEL,
-             r_lower_num = DEFAULT_R_LOWER,
-             r_upper_num = DEFAULT_R_UPPER;
+TEST(TestCases, correctness_case_10) {
+    std::string games_file_path = DEFAULT_GAME_FILE_PATH;
+    std::string temp_file_path = DEFAULT_TEMP_FILE_PATH;
+    int game_num = 10,
+            game_level = DEFAULT_LEVEL,
+            r_lower_num = DEFAULT_R_LOWER,
+            r_upper_num = DEFAULT_R_UPPER;
     genAndSaveGameBoards(game_num, r_lower_num, r_upper_num,
                          game_level, games_file_path);
     std::vector<std::vector<std::vector<int>>> boards = loadBoard(games_file_path);
@@ -178,7 +190,121 @@ TEST(TestCases, correctness_case_5) {
         }
     }
 
- }
+}
+
+TEST(TestCases, range_case_1) {
+    std::string games_file_path = DEFAULT_GAME_FILE_PATH;
+    std::string temp_file_path = DEFAULT_TEMP_FILE_PATH;
+    int game_num = DEFAULT_GAME_NUM,
+            game_level = DEFAULT_LEVEL,
+            r_lower_num = 20,
+            r_upper_num = 40;
+    genAndSaveGameBoards(game_num, r_lower_num, r_upper_num,
+                         game_level, games_file_path);
+    std::vector<std::vector<std::vector<int>>> boards = loadBoard(games_file_path);
+    for (const auto &board : boards) {
+        int num = getEmptyNum(board);
+        ASSERT_TRUE(num >= r_lower_num && num <= r_upper_num);
+    }
+
+}
+
+TEST(TestCases, range_case_2) {
+    std::string games_file_path = DEFAULT_GAME_FILE_PATH;
+    int game_num = DEFAULT_GAME_NUM,
+            game_level = DEFAULT_LEVEL,
+            r_lower_num = 25,
+            r_upper_num = 45;
+    genAndSaveGameBoards(game_num, r_lower_num, r_upper_num,
+                         game_level, games_file_path);
+    std::vector<std::vector<std::vector<int>>> boards = loadBoard(games_file_path);
+    for (const auto &board : boards) {
+        int num = getEmptyNum(board);
+        ASSERT_TRUE(num >= r_lower_num && num <= r_upper_num);
+    }
+
+}
+
+TEST(TestCases, level_case_1) {
+    std::string games_file_path = DEFAULT_GAME_FILE_PATH;
+    int game_num = DEFAULT_GAME_NUM,
+            game_level = 1,
+            r_lower_num = DEFAULT_R_LOWER,
+            r_upper_num = DEFAULT_R_UPPER;
+    std::random_device rd;
+    std::mt19937 rng(rd());
+    std::uniform_int_distribution<int> distribution(r_lower_num, r_upper_num);
+    std::vector<std::vector<std::vector<int>>> sudoku_games;
+    std::vector<int> rs;
+    for (int i = 0; i < game_num; i++) {
+        int r = distribution(rng);
+        rs.push_back(r);
+        std::vector<std::vector<int>> sudoku = genGameBoard(r, game_level);
+        sudoku_games.push_back(sudoku);
+    }
+    for (int i = 0; i < game_num; i++) {
+        int r = rs[i];
+        int step = 0, solutionCount = 0;
+        std::vector<std::vector<int>> copyGrid(sudoku_games[i]);
+        solveSudokuStep(&copyGrid, &step, &solutionCount);
+        ASSERT_TRUE(isInLevel(step, solutionCount, r, game_level));
+    }
+
+}
+
+TEST(TestCases, level_case_2) {
+    std::string games_file_path = DEFAULT_GAME_FILE_PATH;
+    int game_num = DEFAULT_GAME_NUM,
+            game_level = 2,
+            r_lower_num = DEFAULT_R_LOWER,
+            r_upper_num = DEFAULT_R_UPPER;
+    std::random_device rd;
+    std::mt19937 rng(rd());
+    std::uniform_int_distribution<int> distribution(r_lower_num, r_upper_num);
+    std::vector<std::vector<std::vector<int>>> sudoku_games;
+    std::vector<int> rs;
+    for (int i = 0; i < game_num; i++) {
+        int r = distribution(rng);
+        rs.push_back(r);
+        std::vector<std::vector<int>> sudoku = genGameBoard(r, game_level);
+        sudoku_games.push_back(sudoku);
+    }
+    for (int i = 0; i < game_num; i++) {
+        int r = rs[i];
+        int step = 0, solutionCount = 0;
+        std::vector<std::vector<int>> copyGrid(sudoku_games[i]);
+        solveSudokuStep(&copyGrid, &step, &solutionCount);
+        ASSERT_TRUE(isInLevel(step, solutionCount, r, game_level));
+    }
+
+}
+
+TEST(TestCases, level_case_3) {
+    std::string games_file_path = DEFAULT_GAME_FILE_PATH;
+    int game_num = DEFAULT_GAME_NUM,
+            game_level = 3,
+            r_lower_num = DEFAULT_R_LOWER,
+            r_upper_num = DEFAULT_R_UPPER;
+    std::random_device rd;
+    std::mt19937 rng(rd());
+    std::uniform_int_distribution<int> distribution(r_lower_num, r_upper_num);
+    std::vector<std::vector<std::vector<int>>> sudoku_games;
+    std::vector<int> rs;
+    for (int i = 0; i < game_num; i++) {
+        int r = distribution(rng);
+        rs.push_back(r);
+        std::vector<std::vector<int>> sudoku = genGameBoard(r, game_level);
+        sudoku_games.push_back(sudoku);
+    }
+    for (int i = 0; i < game_num; i++) {
+        int r = rs[i];
+        int step = 0, solutionCount = 0;
+        std::vector<std::vector<int>> copyGrid(sudoku_games[i]);
+        solveSudokuStep(&copyGrid, &step, &solutionCount);
+        ASSERT_TRUE(isInLevel(step, solutionCount, r, game_level));
+    }
+
+}
 
 TEST(TestCases, uniqueness_case_1) {
     std::string games_file_path = DEFAULT_TEMP_FILE_PATH;
